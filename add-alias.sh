16be3ffd1ec8dfade477b6a38f9ccb64c352698c
Anedward01 <41715026+anedward01@users.noreply.github.com>
2024-03-03 04:05:20 -0600

    Update add-alias.sh
    Fix error in code

--- add-alias.sh
+++ add-alias.sh
@@ -4,6 +4,6 @@
 # Can be changed to different alias
 
 echo "Adding servers alias to ~/.bash_aliases"
-echo alias servers="sudo tmux -S /usr/servers/Server attach -t Servers" >> ~/.bash_aliases
+echo "alias servers=\"sudo tmux -S /usr/servers/Server attach -t Servers\"" >> ~/.bash_aliases
 
 echo "run \"source ~/.bashrc\" to load new alias"
